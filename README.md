436adafcc00b9a718a4bef8e5b85fb89aac33cf0
Wim Deblauwe <wim.deblauwe@gmail.com>
2024-07-17 17:49:29 +0200

    Implement plugin

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # Vite plugin for Spring Boot backend
 
+## Functionality
+
+This [vite plugin](https://vitejs.dev/guide/api-plugin) has 2 main functionlities to make it possible to use [Spring Boot](https://spring.io/projects/spring-boot) with [Vite](https://vitejs.dev/):
+
+* Copy the HTML templates (Thymeleaf or whatever template engine is used) when the Vite Dev Server is running when they change.
+* Write a file containing the host and port that the Vite Dev Server is running on. This will allow the Spring Boot application to point to the correct URL for the JavaScript and CSS files so [Hot Module Replacement](https://vitejs.dev/guide/features.html#hot-module-replacement) works.
+
+## Usage
+
+Update `vite.config.js` to use the plugin:
+
+```js
+import springBoot from '@wimdeblauwe/vite-plugin-spring-boot';
+
+export default defineConfig({
+  plugins: [
+    springBoot()
+  ],
+});
+```
+
 ## Building
 
 * Run `npm install`
@@ -9,6 +30,8 @@
 
 1. Run `npm link` from this plugin.
 2. Run `npm link @wimdeblauwe/vite-plugin-spring-boot` from the project that would like to use the plugin
+
+During development, run `npm run build-plugin` to have the changes available in the project that uses the plugin.
 
 When done, you can run the following commands:
 
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,106 @@
-import {ViteDevServer} from "vite";
+import {HmrContext, ResolvedConfig, ViteDevServer} from "vite";
+import path from "path";
+import fs from "fs";
+import {globby} from "globby";
 
 export default function springBoot() {
-    return {
-      name: "vite-plugin-spring-boot",
-      configureServer(server: ViteDevServer){
-        console.log("configure server ", server);
+  const targetDir: string = path.resolve(process.cwd(), 'target');
+  const outputDir: string = path.join(targetDir, 'classes');
+  const devServerConfigOutputDir: string = path.join(targetDir, 'vite-plugin-spring-boot');
+  const devServerConfigOutputFile: string = path.join(devServerConfigOutputDir, 'dev-server-config.json');
+
+  let config: ResolvedConfig;
+
+  return {
+    name: "vite-plugin-spring-boot",
+    configResolved(resolvedConfig: ResolvedConfig) {
+      config = resolvedConfig;
+    },
+    async configureServer(server: ViteDevServer) {
+      const rootDir = server.config.root;
+      await copyHtmlFiles(rootDir, outputDir);
+      writeDevServerConfigFile(config, devServerConfigOutputFile);
+      updateDevServerConfigFile(server, devServerConfigOutputFile);
+    },
+    handleHotUpdate(context: HmrContext) {
+      const file: string = context.file;
+      const server: ViteDevServer = context.server;
+      if (path.extname(file) === '.html') {
+        const relativePath = path.relative(server.config.root, file);
+        const outputPath = path.join(outputDir, relativePath);
+        copyFile(file, outputPath, true);
       }
+    },
+    async buildEnd() {
+      const rootDir = config.root;
+      copyHtmlFiles(rootDir, outputDir);
     }
+  }
+}
+
+/**
+ * Writes the static configuration of the Vite Dev Server to a file.
+ */
+function writeDevServerConfigFile(config: ResolvedConfig, devServerConfigOutputFile: string) {
+  const host = config.server.host || 'localhost';
+  const port = config.server.port || 5173;
+  const configData = JSON.stringify({host, port}, null, 2);
+
+  ensureDirectoryExistence(devServerConfigOutputFile);
+  fs.writeFileSync(
+      path.resolve(process.cwd(), devServerConfigOutputFile),
+      configData
+  );
+}
+
+/**
+ * Update the config file with the actual port that was used (This can differ from
+ * the configured port if that port is not free when the Vite Dev Server is started).
+ */
+function updateDevServerConfigFile(server: ViteDevServer, configPath: string) {
+  server.httpServer?.once('listening', () => {
+
+    try {
+      ensureDirectoryExistence(configPath)
+      // Read the existing config file
+      const configData = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+
+      // Update using the actual port that the dev server is running on
+      configData.port = server.config.server.port;
+
+      // Write the updated config back to the file
+      fs.writeFileSync(configPath, JSON.stringify(configData, null, 2));
+    } catch (error) {
+      console.error('Error updating server-config.json:', error);
+    }
+  })
+}
+
+function ensureDirectoryExistence(filePath: string) {
+  const dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+}
+
+function copyFile(src: string, dest: string, logCopy = false) {
+  ensureDirectoryExistence(dest);
+  fs.copyFileSync(src, dest);
+
+  if (logCopy) {
+    console.log(`Copied ${src} to ${dest}`);
+  }
+}
+
+async function copyHtmlFiles(rootDir: string, outputDir: string) {
+  // Copy all HTML files when the server starts
+  const files = await globby('**/*.html', {cwd: rootDir});
+  files.forEach((file: string) => {
+    const srcPath = path.join(rootDir, file);
+    const destPath = path.join(outputDir, file);
+    copyFile(srcPath, destPath);
+  });
+  console.log(`Copied ${files.length} files to ${outputDir}`);
 }
