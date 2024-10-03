6408420fc46a299f0555ffbd431fe1bda18a0b94
Wim Deblauwe <wim.deblauwe@gmail.com>
2024-10-03 08:59:40 +0200

    refactor: Use @rollup/pluginutils instead of minimatch
    This is recommended in the Vite plugin documentation.

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,85 @@
 import {HmrContext, ResolvedConfig, ViteDevServer} from "vite";
+import {createFilter, FilterPattern} from '@rollup/pluginutils';
 import path from "path";
 import fs from "fs";
-import {globby} from "globby";
-import {minimatch} from "minimatch";
+import {globbySync} from "globby";
 
 interface SpringBootOptions {
-  /**
-   * Defines the file paths for files that need to be copied as is when changed. After they are
-   * copied, a full page refresh is done. By default, they are equal to `['**\/*.html', '**\/*.svg']`.
-   */
-  fullCopyFilePaths?: string[];
+    fullCopyFilePaths?: {
+        /**
+         * Defines the file paths for files that need to be copied as is when changed. After they are
+         * copied, a full page refresh is done. By default, they are equal to `['**\/*.html', '**\/*.svg']`
+         */
+        include?: FilterPattern;
+        /**
+         * Defines patterns to exclude files from being processed. No files are excluded by default.
+         */
+        exclude?: FilterPattern;
+    };
+
+    /**
+     * Enable verbose logging of the copy actions this plugin does
+     */
+    verbose?: boolean;
 }
 
 export default function springBoot(options: SpringBootOptions = {}) {
-  const targetDir: string = path.resolve(process.cwd(), 'target');
-  const outputDir: string = path.join(targetDir, 'classes');
-  const devServerConfigOutputDir: string = path.join(targetDir, 'vite-plugin-spring-boot');
-  const devServerConfigOutputFile: string = path.join(devServerConfigOutputDir, 'dev-server-config.json');
+    const targetDir: string = path.resolve(process.cwd(), 'target');
+    const outputDir: string = path.join(targetDir, 'classes');
+    const devServerConfigOutputDir: string = path.join(targetDir, 'vite-plugin-spring-boot');
+    const devServerConfigOutputFile: string = path.join(devServerConfigOutputDir, 'dev-server-config.json');
 
-  const filePathsToHandle = options.fullCopyFilePaths || ['**/*.html', '**/*.svg'];
+    const filter = createFilter(
+        options.fullCopyFilePaths?.include || ['**/*.html', '**/*.svg'],
+        options.fullCopyFilePaths?.exclude
+    );
 
-  let config: ResolvedConfig;
+    const verbose: boolean = options.verbose || false;
 
-  return {
-    name: "vite-plugin-spring-boot",
-    configResolved(resolvedConfig: ResolvedConfig) {
-      config = resolvedConfig;
-    },
-    async configureServer(server: ViteDevServer) {
-      const rootDir = server.config.root;
-      await copyFiles(filePathsToHandle, rootDir, outputDir);
-      writeDevServerConfigFile(config, devServerConfigOutputFile);
-      updateDevServerConfigFile(server, devServerConfigOutputFile);
-    },
-    handleHotUpdate(context: HmrContext) {
-      const file: string = context.file;
-      const server: ViteDevServer = context.server;
-      if (filePathsToHandle.some(pattern => matchGlobPattern(pattern, file, server.config.root))) {
-        const relativePath = path.relative(server.config.root, file);
-        const outputPath = path.join(outputDir, relativePath);
-        copyFile(file, outputPath, true);
-        // Force a full page reload when a file was updated
-        server.ws.send({ type: "full-reload" });
-      }
-    },
-    async buildEnd() {
-      const rootDir = config.root;
-      copyFiles(filePathsToHandle, rootDir, outputDir);
+    let config: ResolvedConfig;
+
+    return {
+        name: "vite-plugin-spring-boot",
+        configResolved(resolvedConfig: ResolvedConfig) {
+            config = resolvedConfig;
+        },
+        async configureServer(server: ViteDevServer) {
+            const rootDir = server.config.root;
+            await copyFiles(filter, rootDir, outputDir, verbose);
+            writeDevServerConfigFile(config, devServerConfigOutputFile);
+            updateDevServerConfigFile(server, devServerConfigOutputFile);
+        },
+        handleHotUpdate(context: HmrContext) {
+            const file: string = context.file;
+            const server: ViteDevServer = context.server;
+            if (filter(file)) {
+                const relativePath = path.relative(server.config.root, file);
+                const outputPath = path.join(outputDir, relativePath);
+                copyFile(file, outputPath, true);
+                // Force a full page reload when a file was updated
+                server.ws.send({type: "full-reload"});
+            }
+        },
+        async buildEnd() {
+            const rootDir = config.root;
+            copyFiles(filter, rootDir, outputDir, verbose);
+        }
     }
-  }
-}
-
-function matchGlobPattern(pattern: string, filePath: string, rootDir: string): boolean {
-  const relativePath = path.relative(rootDir, filePath);
-  return minimatch(relativePath, pattern);
 }
 
 /**
  * Writes the static configuration of the Vite Dev Server to a file.
  */
 function writeDevServerConfigFile(config: ResolvedConfig, devServerConfigOutputFile: string) {
-  const host = config.server.host || 'localhost';
-  const port = config.server.port || 5173;
-  const configData = JSON.stringify({host, port}, null, 2);
+    const host = config.server.host || 'localhost';
+    const port = config.server.port || 5173;
+    const configData = JSON.stringify({host, port}, null, 2);
 
-  ensureDirectoryExistence(devServerConfigOutputFile);
-  fs.writeFileSync(
-      path.resolve(process.cwd(), devServerConfigOutputFile),
-      configData
-  );
+    ensureDirectoryExistence(devServerConfigOutputFile);
+    fs.writeFileSync(
+        path.resolve(process.cwd(), devServerConfigOutputFile),
+        configData
+    );
 }
 
 /**
@@ -76,50 +87,58 @@ function writeDevServerConfigFile(config: ResolvedConfig, devServerConfigOutputF
  * the configured port if that port is not free when the Vite Dev Server is started).
  */
 function updateDevServerConfigFile(server: ViteDevServer, configPath: string) {
-  server.httpServer?.once('listening', () => {
+    server.httpServer?.once('listening', () => {
 
-    try {
-      ensureDirectoryExistence(configPath)
-      // Read the existing config file
-      const configData = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+        try {
+            ensureDirectoryExistence(configPath)
+            // Read the existing config file
+            const configData = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 
-      // Update using the actual port that the dev server is running on
-      configData.port = server.config.server.port;
+            // Update using the actual port that the dev server is running on
+            configData.port = server.config.server.port;
 
-      // Write the updated config back to the file
-      fs.writeFileSync(configPath, JSON.stringify(configData, null, 2));
-    } catch (error) {
-      console.error('Error updating server-config.json:', error);
-    }
-  })
+            // Write the updated config back to the file
+            fs.writeFileSync(configPath, JSON.stringify(configData, null, 2));
+        } catch (error) {
+            console.error('Error updating server-config.json:', error);
+        }
+    })
 }
 
 function ensureDirectoryExistence(filePath: string) {
-  const dirname = path.dirname(filePath);
-  if (fs.existsSync(dirname)) {
-    return true;
-  }
-  ensureDirectoryExistence(dirname);
-  fs.mkdirSync(dirname);
+    const dirname = path.dirname(filePath);
+    if (fs.existsSync(dirname)) {
+        return true;
+    }
+    ensureDirectoryExistence(dirname);
+    fs.mkdirSync(dirname);
 }
 
 function copyFile(src: string, dest: string, logCopy = false) {
-  ensureDirectoryExistence(dest);
-  fs.copyFileSync(src, dest);
+    ensureDirectoryExistence(dest);
+    fs.copyFileSync(src, dest);
 
-  if (logCopy) {
-    console.log(`Copied ${src} to ${dest}`);
-  }
+    if (logCopy) {
+        console.log(`Copied ${src} to ${dest}`);
+    }
 }
 
-async function copyFiles(patterns: string[], rootDir: string, outputDir: string) {
-  const files = await globby(patterns, {cwd: rootDir});
-  files.forEach((file: string) => {
-    const srcPath = path.join(rootDir, file);
-    const destPath = path.join(outputDir, file);
-    copyFile(srcPath, destPath);
-  });
-  if (files.length > 0) {
-      console.log(`Copied ${files.length} files to ${outputDir}`);
-  }
+async function copyFiles(filter: (id: string | unknown) => boolean, rootDir: string, outputDir: string, verbose: boolean) {
+    const files = globbySync('**/*', {cwd: rootDir});
+    if (verbose) {
+        console.log(`Found ${files.length} files at ${rootDir}, filtering and copying...`);
+    }
+
+    let count = 0;
+    files.forEach((file: string) => {
+        const srcPath = path.join(rootDir, file);
+        if (filter(srcPath)) {
+            const destPath = path.join(outputDir, file);
+            copyFile(srcPath, destPath, verbose);
+            count++;
+        }
+    });
+    if (count > 0) {
+        console.log(`Copied ${count} files to ${outputDir}`);
+    }
 }
